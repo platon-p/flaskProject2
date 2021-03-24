3e13c8451d878ef70e68911d90d93a31eb31a6b3
platon <platonpechenev@yandex.ru>
2021-03-24 18:40:06 +0300

    .

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('ind.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('reg.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
